df71e4bf85b380946bd972195d551f25f34348fc
Franklin <franklin.carrero@conekta.com>
2022-02-03 11:03:40 -0500

    added | xargs for scape spaces

--- commits-count.sh
+++ commits-count.sh
@@ -7,6 +7,6 @@ target=$2
 sourcePath=$(git branch -r | grep "$source" | xargs)
 targetPath=$(git branch -r | grep "$target" | xargs)
 
-commitsCount=$(git log --oneline "$sourcePath" \^"$targetPath" | wc -l)
+commitsCount=$(git log --oneline "$sourcePath" \^"$targetPath" | wc -l | xargs)
 
 echo "$commitsCount"
